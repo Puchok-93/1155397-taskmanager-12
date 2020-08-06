6113355356943f49fdfc5de14c74fb4f5a00ae2b
Puchok-93 <paul.puchkov@yandex.ru>
2020-08-06 12:33:17 +0300

    Создание моковых данных

--- src/main.js
+++ src/main.js
@@ -1,33 +1,51 @@
-import {createMainControlTemplate} from "./view/main-control.js";
+import {createSiteMenuTemplate} from "./view/main-control.js";
 import {createFilterTemplate} from "./view/filter.js";
+import {createTaskTemplate} from "./view/task.js";
+import {createTaskEditTemplate} from "./view/edit-task.js";
 import {createBoardTemplate} from "./view/board.js";
 import {createSortTemplate} from "./view/sort.js";
-import {createEditTaskTemplate} from "./view/edit-task.js";
-import {createTaskTemplate} from "./view/task.js";
+import {generateTask} from "./mock/task.js";
+import {generateFilter} from "./mock/filter.js";
+import {render} from "./utils.js";
+import {TASK_COUNT, TASK_COUNT_PER_STEP} from "./const.js";
 
-const TASK_COUNT = 3;
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const siteMain = document.querySelector(`.main`);
 const siteHeaderElement = siteMain.querySelector(`.main__control`);
+const createMoreTaskButtonTemplate = () => `<button class="load-more" type="button">load more</button>`;
 
-const createMoreTaskButtonTemplate = () => ` <button class="load-more" type="button">load more</button>`;
+render(siteHeaderElement, createSiteMenuTemplate());
+render(siteMain, createFilterTemplate(filters));
+render(siteMain, createBoardTemplate());
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
+const boardElement = siteMain.querySelector(`.board`);
+const taskListElement = boardElement.querySelector(`.board__tasks`);
+render(boardElement, createSortTemplate(), `afterbegin`);
+render(taskListElement, createTaskEditTemplate(tasks[0]));
 
-render(siteHeaderElement, createMainControlTemplate(), `beforeend`);
-render(siteMain, createFilterTemplate(), `beforeend`);
-render(siteMain, createBoardTemplate(), `beforeend`);
-
-const boardElement = document.querySelector(`.board`);
-const taskListElement = document.querySelector(`.board__tasks`);
-
-render(boardElement, createSortTemplate(), `afterBegin`);
-render(taskListElement, createEditTaskTemplate(), `beforeend`);
-
-for (let i = 0; i <= TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate(), `beforeend`);
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]));
 }
 
-render(taskListElement, createMoreTaskButtonTemplate(), `beforeend`);
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+  render(boardElement, createMoreTaskButtonTemplate());
+
+  const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .forEach((task) => render(taskListElement, createTaskTemplate(task)));
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
---
+++ src/mock/task.js
@@ -0,0 +1,72 @@
+import {COLORS} from "../const";
+import {getRandomInteger} from "../utils";
+
+/* Генерируем случайное описание */
+const generateDescription = () => {
+  const descriptions = [`Изучить теорию`, `Сделать домашку`, `Пройти интенсив на соточку`, `test1`, `test2`];
+  const randomIndex = getRandomInteger(0, descriptions.length - 1);
+
+  return descriptions[randomIndex];
+};
+
+/* Генерируем случайное дату */
+
+const generateDate = () => {
+
+  // Установлена ли дата
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+  const currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate.setDate(currentDate.getDate() + daysGap);
+  return new Date(currentDate);
+};
+
+/* Генерируем повторяющиеся дни */
+
+const generateRepeating = () => {
+  return {
+    mo: Boolean(getRandomInteger(0, 1)),
+    tu: false,
+    we: false,
+    th: false,
+    fr: false,
+    sa: false,
+    su: Boolean(getRandomInteger(0, 1))
+  };
+};
+
+/* Генерируем случайный цвет */
+const getRandomColor = () => {
+  const randomIndex = getRandomInteger(0, COLORS.length - 1);
+
+  return COLORS[randomIndex];
+};
+
+export const generateTask = () => {
+  const dueDate = generateDate();
+  const repeating = dueDate === null ? generateRepeating() : {
+    mo: false,
+    tu: false,
+    we: false,
+    th: false,
+    fr: false,
+    sa: false,
+    su: false
+  };
+
+  return {
+    description: generateDescription(),
+    dueDate,
+    repeating,
+    color: getRandomColor(),
+    isArchive: Boolean(getRandomInteger(0, 1)),
+    isFavorite: Boolean(getRandomInteger(0, 1))
+  };
+};
