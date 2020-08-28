44c770a9debb319482064a6476d286cc3b8522ed
Puchok-93 <paul.puchkov@yandex.ru>
2020-08-29 00:52:17 +0300

    Добавление сортировки задач

--- src/const.js
+++ src/const.js
@@ -1,3 +1,8 @@
 export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
 export const TASK_COUNT = 25;
 export const TASK_COUNT_PER_STEP = 8;
+export const SortType = {
+  DEFAULT: `default`,
+  DATE_UP: `date-up`,
+  DATE_DOWN: `date-down`
+};
