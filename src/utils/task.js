b891a827dfdf1f9a64211cd8dcacfeb09873c4e7
Puchok-93 <paul.puchkov@yandex.ru>
2020-09-08 23:17:04 +0300

    Подключение библиотек flatpickr и moment

--- src/utils/task.js
+++ src/utils/task.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 const getCurrentDate = () => {
   const currentDate = new Date();
   currentDate.setHours(23, 59, 59, 999);
@@ -19,8 +21,12 @@ export const isTaskRepeating = (repeating) => {
   return Object.values(repeating).some(Boolean);
 };
 
-export const humanizeTaskDueDate = (dueDate) => {
-  return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
+export const formatTaskDueDate = (dueDate) => {
+  if (!(dueDate instanceof Date)) {
+    return ``;
+  }
+
+  return moment(dueDate).format(`D MMMM`);
 };
 
 export const isTaskExpiringToday = (dueDate) => {
--- src/view/edit-task.js
+++ src/view/edit-task.js
@@ -1,6 +1,8 @@
 import Smart from "./smart.js";
 import {COLORS} from "../const.js";
-import {isTaskExpired, isTaskRepeating, humanizeTaskDueDate} from "../utils/task.js";
+import {isTaskRepeating, formatTaskDueDate} from "../utils/task.js";
+import flatpickr from "flatpickr";
+import "../../node_modules/flatpickr/dist/flatpickr.min.css";
 
 const DEFAULT_TASK = {
   color: COLORS[0],
@@ -30,7 +32,7 @@ const createTaskEditDateTemplate = (dueDate, isDueDate) => {
           type="text"
           placeholder=""
           name="date"
-          value="${dueDate !== null ? humanizeTaskDueDate(dueDate) : ``}"
+          value="${formatTaskDueDate(dueDate)}"
         />
       </label>
     </fieldset>` : ``}
@@ -76,17 +78,15 @@ const createTaskEditColorsTemplate = (currentColor) => {
 
 const createTaskEditTemplate = (data) => {
   const {color, description, dueDate, repeating, isDueDate, isRepeating} = data;
-  const deadlineClassName = isTaskExpired(dueDate)
-    ? `card--deadline`
-    : ``;
+
   const dateTemplate = createTaskEditDateTemplate(dueDate, isDueDate);
   const repeatingClassName = isRepeating
     ? `card--repeat`
     : ``;
   const repeatingTemplate = createTaskEditRepeatingTemplate(repeating, isRepeating);
   const colorsTemplate = createTaskEditColorsTemplate(color);
-  const isSubmitDisabled = isRepeating && !isTaskRepeating(repeating);
-  return `<article class="card card--edit card--${color} ${deadlineClassName} ${repeatingClassName}">
+  const isSubmitDisabled = (isDueDate && dueDate === null) || (isRepeating && !isTaskRepeating(repeating));
+  return `<article class="card card--edit card--${color} ${color} ${repeatingClassName}">
     <form class="card__form" method="get">
       <div class="card__inner">
         <div class="card__color-bar">
@@ -130,14 +130,16 @@ export default class TaskEdit extends Smart {
   constructor(task = DEFAULT_TASK) {
     super();
     this._data = TaskEdit.parseTaskToData(task);
-
+    this._datepicker = null;
     this._formSubmitHandler = this._formSubmitHandler.bind(this);
     this._dueDateToggleHandler = this._dueDateToggleHandler.bind(this);
     this._repeatingToggleHandler = this._repeatingToggleHandler.bind(this);
     this._descriptionInputHandler = this._descriptionInputHandler.bind(this);
     this._repeatingChangeHandler = this._repeatingChangeHandler.bind(this);
     this._colorChangeHandler = this._colorChangeHandler.bind(this);
+    this._dueDateChangeHandler = this._dueDateChangeHandler.bind(this);
     this._setInnerHandlers();
+    this._setDatePicker();
   }
 
   getTemplate() {
@@ -154,7 +156,26 @@ export default class TaskEdit extends Smart {
 
   restoreHandlers() {
     this._setInnerHandlers();
+    this._setDatePicker();
     this.setFormSubmitHandler(this._callback.formSubmit);
+  }
+
+  _setDatePicker() {
+    if (this._datepicker) {
+      this._datepicker.destroy();
+      this._datepicker = null;
+    }
+
+    if (this._data.isDueDate) {
+      this._datepicker = flatpickr(
+          this.getElement().querySelector(`.card__date`),
+          {
+            dateFormat: `j F`,
+            defaultDate: this._data.dueDate,
+            onChange: this._dueDateChangeHandler
+          }
+      );
+    }
   }
 
   /* Навешиваем обработчики на элементы управления задачами */
@@ -192,6 +213,14 @@ export default class TaskEdit extends Smart {
     this.updateData({
       description: evt.target.value
     }, true);
+  }
+
+  _dueDateChangeHandler([userDate]) {
+    userDate.setHours(23, 59, 59, 999);
+
+    this._updateData({
+      dueDate: userDate
+    });
   }
 
   _repeatingChangeHandler(evt) {
